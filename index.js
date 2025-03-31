4c7f803465ab60a850567770f4eb941308869b8b
Caroline Ininga <carolineininga@gmail.com>
2025-03-31 10:14:17 +0300

    completed dom contentlooaded lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+//set up DOMContentloaded 
+
+document.addEventListener('DOMContentLoaded',
+() => {document.querySelector('#text').textContent =
+     "This is really cool!"
+    
+    }
+     )
+
+     console.log("this is outside")
+
+     console.log(document.querySelector('#text'))
